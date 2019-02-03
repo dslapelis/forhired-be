e607fc7953a7779024cf51a24767d46c853e0283
Daniel J. Slapelis <djs7209@rit.edu>
2019-02-03 14:42:56 -0500

    S3 upload resume

--- proj/api/models/resume.py
+++ proj/api/models/resume.py
@@ -4,6 +4,18 @@ from authentication.models import Company
 from datetime import datetime
 
 
+def upload_resume_to(instance, filename):
+    import os
+    from django.utils.timezone import now
+    _, filename_ext = os.path.splitext(filename)
+    return 'resumes/%s%s' % (
+        now().strftime("%Y%m%d%H%M%S"),
+        filename_ext.lower(),
+    )
+
+
 class Resume(models.Model):
-    resume = models.FileField(upload_to='forhired-resumes')
+    resume = models.FileField(upload_to=upload_resume_to, null=False)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=False, blank=False)
     date_posted = models.DateTimeField(auto_now_add=True, blank=True)
--- proj/api/tests.py
+++ proj/api/tests.py
@@ -2,6 +2,7 @@ from os.path import abspath, join, dirname
 from shutil import rmtree
 from tempfile import mkdtemp
 from django.core.files.storage import FileSystemStorage
+from django.core.files.uploadedfile import SimpleUploadedFile, UploadedFile
 from django.urls import reverse
 from django.test import TestCase
 from django.test.client import Client
@@ -21,17 +22,10 @@ class ViewTest(TestCase):
     def tearDown(self):
         rmtree(self.media_folder)
 
-    @mock.patch('storages.backends.s3boto.S3BotoStorage', FileSystemStorage)
     def test_post_resume(self):
         resume_path = join(abspath(dirname(__file__)),
-                           'fixtures/DanielSlapelisResume-2019.pages')
-
-        s3 = boto3.client('s3', aws_access_key_id=AWS_S3_ACCESS_KEY_ID,
-                          aws_secret_access_key=AWS_S3_SECRET_ACCESS_KEY)
-        s3.upload_file(resume_path,
-                       AWS_STORAGE_BUCKET_NAME, "danielslapelis-resume")
-        resume = Resume(resume=resume_path)
-        try:
-            assertTrue(resume.save())
-        except:
-            pass
+                           'fixtures/DanielSlapelisResume-2019.docx')
+        resume = Resume()
+        resume.name = 'oogity boogity'
+        resume.resume = UploadedFile(open(resume_path, 'rb'))
+        resume.save()
